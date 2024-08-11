0b45c4143bf8417bd9f191d99894f08cd82af8f0
Nick Colvin <njc@alumni.unc.edu>
2024-08-11 14:56:21 -0400

    add loop, fix last minute of block

--- app.py
+++ app.py
@@ -8,57 +8,56 @@ level_terms = {
     "easy": "beginner",
     "medium": "intermediate",
     "hard": "advanced",
-    "very hard": "expert",
 }
-level = None
-while level not in ("easy", "medium", "hard", "very hard"):
-    level = input("Enter the difficulty of your ride. (easy, medium, hard, very hard): ")
-level = level_terms[level]
-
-# get the users ride duration
-length = None
-while length not in (10, 20, 30, 45, 60):
-    length = int(input("Enter the duration of your ride in minutes. (10, 20, 30, 45, 60): "))
-
-# get the users ride category
-category = None
-while category not in ("cadence", "power", "hills", "fusion"):
-    category = input("Enter the category of your ride. (cadence, power, hills, fusion): ")
-
-# generate a class with AI
-client = Anthropic(
-    # This is the default and can be omitted
-    api_key=os.environ.get("ANTHROPIC_API_KEY"),
-)
-message = client.messages.create(
-    max_tokens=1024,
-    messages=[
-        {
-            "role": "user",
-            "content": f"You are a personal trainer. Write a minute-by-minute {level} level {length} minute {category} cycling class with exact cadence and resistance ranges for every block. Warm-up and cool down should be no more than 3 minutes long. Cadence is in units of RPM, ranges should be between 5-10 units, the minimum cadence is 45 and maximum is 125.  Resistance is a 32 level scale and ranges should be 5 units. Format each part as follows:\n\n<block_title>\nTime: <start_minute>-<end_minute>\nCadence: <cadence_low>-<cadence_high>\nResistance: <resistance_low>-<resistance_high>\n\nDo not include any other information in your message besides the blocks of your class.",
-        }
-    ],
-    model="claude-3-5-sonnet-20240620",
-)
-
-spin_class = "\n".join([
-    block.text for block in message.content if block.type == "text"
-])
-
-print(spin_class)
-# split the class up by double line breaks
-spin_class_split = spin_class.split("\n\n")
-# TTS engine
-engine = pyttsx3.init()
-
-engine.say("Here is your class. Let me know when you are ready to begin.")
-engine.runAndWait()
 
 ready = False
 while not ready:
-    user_ready = input("\nReady? (y/n/exit): ")
+    level = None
+    while level not in ("easy", "medium", "hard"):
+        level = input("Enter the difficulty of your ride. (easy, medium, hard): ")
+    level = level_terms[level]
+
+    # get the users ride duration
+    length = None
+    while length not in (10, 20, 30, 45, 60):
+        length = int(input("Enter the duration of your ride in minutes. (10, 20, 30, 45, 60): "))
+
+    # get the users ride category
+    category = None
+    while category not in ("cadence", "power", "hills", "fusion"):
+        category = input("Enter the category of your ride. (cadence, power, hills, fusion): ")
+
+    # generate a class with AI
+    client = Anthropic(
+        # This is the default and can be omitted
+        api_key=os.environ.get("ANTHROPIC_API_KEY"),
+    )
+    message = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": f"You are a personal trainer. Write a minute-by-minute {level} level {length} minute {category} cycling class with exact cadence and resistance ranges for every block. Warm-up and cool down should be between 1 and {length//10} minutes long. Cadence is in units of RPM, ranges should be between 5-10 units, the minimum cadence is 45 and maximum is 125.  Resistance is a 32 level scale and ranges should be 5 units. Format each block as follows:\n\n<block_title>\nTime: <start_minute>-<end_minute>\nCadence: <cadence_low>-<cadence_high>\nResistance: <resistance_low>-<resistance_high>\n\nDo not include any other information in your message besides the blocks of your class.",
+            }
+        ],
+        model="claude-3-5-sonnet-20240620",
+    )
+
+    spin_class = "\n".join([
+        block.text for block in message.content if block.type == "text"
+    ])
+
+    print(spin_class)
+    # split the class up by double line breaks
+    spin_class_split = spin_class.split("\n\n")
+    # TTS engine
+    engine = pyttsx3.init()
+
+    engine.say("Here is your class. Let me know when you are ready to begin.")
+    engine.runAndWait()
+    user_ready = input("\nReady? (y/n/q(uit)): ")
     ready = user_ready == "y"
-    if user_ready == "exit":
+    if user_ready == "q":
         quit()
 
 engine.say("Let's go!")
@@ -106,19 +105,20 @@ for block in spin_class_split:
         engine.runAndWait()
         time.sleep(1)
 
-    engine.say("Begin")
+    engine.say("begin")
     engine.runAndWait()
 
     block_duration = end_time - start_time
     # announce each minute of the block, 30 seconds, 10 seconds, 5-4-3-2-1 countdown
-    for i in range(block_duration - 1, 1, -1):
+    if block_duration > 1:
+        for i in range(block_duration - 1, 1, -1):
+            time.sleep(60)
+            engine.say(f"{i} minutes left")
+            engine.runAndWait()
+        
         time.sleep(60)
-        engine.say(f"{i} minutes left")
+        engine.say("1 minute left")
         engine.runAndWait()
-    
-    time.sleep(60)
-    engine.say("1 minute left")
-    engine.runAndWait()
 
     time.sleep(30)
     engine.say("30 seconds left")
